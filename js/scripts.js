d945acaf8ac367512572f796b073ae58add26b20
Maria Lagunero <maria58@gmail.com>
2020-03-31 10:28:14 -0700

    added variables and forEach methods

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $("#submit").click(function(event){
+    event.preventDefault();
+    var numbers = [2,3,4,5,6,7,8,9,10];
+    var letters = ["ace", "jack", "queen", "king"]
+    var suites = ["clubs", "spades", "hearts", "diamonds"];
+    // var numOutput = [];
+    
+    suites.forEach(function(suite){
+      numbers.forEach(function(number){
+        var numDeck = number + " of " + suite;
+        console.log(numDeck);
+      });
+      letters.forEach(function(letter){
+        var letterDeck = letter + " of " + suite;
+        console.log(letterDeck);
+      });
+    });
+
+
+
+   // [number] of [suite]
+   // [letter] of [suite]
+  });
+});
+// var deck = [a,2,3,4,5,6,7,8,9,0,10,j,q,k];
+
+// var suites = [];
+// numbers.forEach(function(number) {
+  
+
+
+
+
+
+
+
+
